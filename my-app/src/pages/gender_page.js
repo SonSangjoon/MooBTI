c9de97e9e0561b48b54fdc7f0c82a41cd18cc7e8
onshore <onshorecoding9@gmail.com>
2021-04-02 18:41:19 +0900

    [ADD] Add Material UI &  Create Background Color

--- my-app/src/pages/gender_page.js
+++ my-app/src/pages/gender_page.js
@@ -1,7 +1,12 @@
 import React from 'react'
+import Grid from '@material-ui/core/Grid';
+import Box from '@material-ui/core/Box';
 
-export function genderPage() {
+export function GenderPage() {
     return (
-        "gender"
+        <Box>
+            <Grid container>
+            </Grid>
+        </Box>
     )
 }
