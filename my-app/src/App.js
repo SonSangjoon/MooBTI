bf1b3f99c93001b6d55a5842b3e9def1dade1090
onshore <onshorecoding9@gmail.com>
2021-04-02 16:02:02 +0900

    [ADD] build react directory

--- my-app/src/App.js
+++ my-app/src/App.js
@@ -1,23 +1,19 @@
-import logo from './logo.svg';
-import './App.css';
 
-function App() {
+import { Route, BrowserRouter  } from 'react-router-dom';
+import { introPage } from './pages/intro_page'
+import { testPage } from './pages/test_page'
+import { genderPage } from './pages/gender_page'
+import { resultPage } from './pages/result_page'
+
+export function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div>
+      <BrowserRouter>
+          <Route exact path="/" component={introPage}/>
+          <Route exact path="/test" component={testPage}/>
+          <Route exact path="/gender"component={genderPage}/>
+          <Route exact path="/:mbti/:gender"component={resultPage}/>
+      </BrowserRouter>
     </div>
   );
 }
