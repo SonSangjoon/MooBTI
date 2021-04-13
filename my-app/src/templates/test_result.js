0412589f47e1e636efcbf54925f8bcfbb4156dcf
YUZAMIN <untmd.crmn@gmail.com>
2021-04-13 20:30:52 +0900

    <feat> : Add font & question contents change 결과 페이지 폰트 크기 미적용, 수정 예정

--- my-app/src/templates/test_result.js
+++ my-app/src/templates/test_result.js
@@ -1,5 +1,5 @@
 import React from "react";
-import { Button, Typography, Grid } from "@material-ui/core";
+import { Button, Typography, Grid, Box } from "@material-ui/core";
 import dummy_img from "../images/result/dummy_titanic_jack.jpg";
 import { withStyles, makeStyles,} from "@material-ui/core/styles";
 
@@ -102,9 +102,9 @@ function ResultMain() {
         <img className={classes.characterImage} src={dummy_img} alt="dummy" />
       </Grid>
       <Grid className={classes.characterTitleGrid}>
-        <Typography className={classes.characterTitle} align="center">
+        <Box className={classes.characterTitle} align="center">
           항상 사랑을 말하는 당신은, 타이타닉의 Jack
-        </Typography>
+        </Box>
       </Grid>
     </Grid>
   );
@@ -115,16 +115,16 @@ function ResultDescription() {
   return (
     <Grid className={classes.container}>
         <Grid className={classes.characterScript} item>
-          <Typography className={classes.charScriptFont} align="center">
+          <Box className={classes.charScriptFont} align="center">
             "이 배의 탑승권을 따낸 건 내 인생 최고의 행운이었어. 당신을 만났으니까"
-          </Typography>
+          </Box>
         </Grid>
         <Grid className={classes.characterDetail}>
-          <Typography align="center" className={classes.characterDetailText} >
+          <Box align="center" className={classes.characterDetailText} >
           외향적이고 감성적인 ESFP 유형의 사람들은 낙천적이고 관계 맺기를 좋아해요. 그래서 연애할 때 상대방에게 사랑을 시도 때도 없이 표현하곤 하죠 😘 
           종종 여자친구에게 자신의 가치관을 이야기해요. 상대방이 자신을 그대로 인정해준다면 온화해지지만, 관계가 조금이라도 불편해진다면 빠르게 거리를 둔답니다. 
           거부당하는 걸 두려워하는 당신에게, 해주고 싶은 말이 있어요. "사랑하는 사람에게 기꺼이 상처 받을 것"이라는 말이에요.
-          </Typography>
+          </Box>
         </Grid>
     </Grid>
   );
