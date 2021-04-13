38b8e93d1969893045cd8abd3b9e58435e62f56a
YUZAMIN <untmd.crmn@gmail.com>
2021-04-14 02:23:18 +0900

    <feat> : table view layout changing 미완성, 작업중

--- my-app/src/pages/gender_page.js
+++ my-app/src/pages/gender_page.js
@@ -5,13 +5,13 @@ import female_pic from "../images/gender/female_pic.jpg";
 import male_pic from "../images/gender/male_pic.jpg";
 import NavBar from "../components/nav_bar";
 import { makeStyles } from "@material-ui/core/styles";
-import {  Button , Typography, Box, Grid  } from '@material-ui/core';
-import { Mobile, Tablet, PC, PCwide } from '../components/MediaQuery' 
+import { Button, Typography, Box, Grid } from "@material-ui/core";
+import { Mobile, Tablet, PC, PCwide } from "../components/MediaQuery";
 
 const useStyles = makeStyles((theme) => ({
   root: {
-    height: '100vh',
-    backgroundColor: 'black'
+    height: "100vh",
+    backgroundColor: "black",
   },
 
   container: {
@@ -30,32 +30,63 @@ const useStyles = makeStyles((theme) => ({
   },
 
   title: {
-    paddingBottom: '1vh',
-    fontSize: '28px',
-    color: 'white',
+    paddingBottom: "1vh",
+    fontSize: "28px",
+    color: "white",
   },
 
   imgTitle: {
     fontSize: 20,
-    color: 'white',
+    color: "white",
   },
 
   gender_img: {
-    borderRadius:'10px',
-    '&:hover': {
+    borderRadius: "10px",
+    "&:hover": {
       backgroundColor: "white",
-      borderRadius:'10px'
-
+      borderRadius: "10px",
     },
-    padding: '4px 4px',
-    marginBottom: '10px' 
+    padding: "4px 4px",
+    marginBottom: "10px",
   },
 
-  image: { 
-    borderRadius:'10px',
+  image: {
+    borderRadius: "10px",
   },
 
-//Tablet Layout
+  //Tablet Layout
+
+  tabletRoot: {
+    height: "100vh",
+    backgroundColor: "black",
+  },
+
+  tabletContainer: {
+    height: "100vh",
+    display: "grid",
+    gridTemplateRows: "repeat(3, 1fr)",
+    gridTemplateColumns: "1fr 2fr 1fr",
+    gridGap: theme.spacing(2),
+  },
+
+  // tabletEmptyRowGrid: {
+  //   gridRow: "span 2",
+  // },
+  // tabletEmptyColumnGrid: {
+  //   gridColumn: "span 2",
+  // },
+
+  tabletGenderPageGrid:{
+    gridColumn: "2",
+    gridRow: "2"
+  },
+
+
+  tabletTitle:{
+    fontSize: "2.7vw",
+    color: "white",
+
+  },
 
 }));
 
@@ -76,21 +107,43 @@ function GenderPageSelect() {
       <Box display="flex" justifyContent="center" mt={3}>
         <Box mr={5}>
           <Link to="/test">
-            <Button className={classes.gender_img} value="male" onClick={handleGenderChange}>
+            <Button
+              className={classes.gender_img}
+              value="male"
+              onClick={handleGenderChange}
+            >
               <img src={male_pic} alt="male_pic" className={classes.image} />
             </Button>
           </Link>
-          <Box className={classes.imgTitle} variant="h6" align="center" gutterBottom>
+          <Box
+            className={classes.imgTitle}
+            variant="h6"
+            align="center"
+            gutterBottom
+          >
             남성
           </Box>
         </Box>
         <Box>
           <Link to="/test">
-          <Button className={classes.gender_img} value="female" onClick={handleGenderChange}>
-              <img src={female_pic} alt="female_pic" className={classes.image}/>
+            <Button
+              className={classes.gender_img}
+              value="female"
+              onClick={handleGenderChange}
+            >
+              <img
+                src={female_pic}
+                alt="female_pic"
+                className={classes.image}
+              />
             </Button>
           </Link>
-          <Box className={classes.imgTitle} variant="h6" align="center" gutterBottom>
+          <Box
+            className={classes.imgTitle}
+            variant="h6"
+            align="center"
+            gutterBottom
+          >
             여성
           </Box>
         </Box>
@@ -99,46 +152,116 @@ function GenderPageSelect() {
   );
 }
 
+
+
+function GenderPageSelectMobile() {
+  const classes = useStyles();
+  const { gender, setGender } = useContext(GenderContext);
+
+  const handleGenderChange = (event) => {
+    setGender(event.currentTarget.value);
+    console.log(gender);
+  };
+
+  return (
+    <Box>
+      <Box className={classes.tabletTitle} align="center" gutterBottom>
+        테스트를 진행할 프로필을 선택하세요.
+      </Box>
+      <Box display="flex" justifyContent="center" mt={3}>
+        <Box mr={5}>
+          <Link to="/test">
+            <Button
+              className={classes.gender_img}
+              value="male"
+              onClick={handleGenderChange}
+            >
+              <img src={male_pic} alt="male_pic" className={classes.image} />
+            </Button>
+          </Link>
+          <Box
+            className={classes.imgTitle}
+            variant="h6"
+            align="center"
+            gutterBottom
+          >
+            남성
+          </Box>
+        </Box>
+        <Box>
+          <Link to="/test">
+            <Button
+              className={classes.gender_img}
+              value="female"
+              onClick={handleGenderChange}
+            >
+              <img
+                src={female_pic}
+                alt="female_pic"
+                className={classes.image}
+              />
+            </Button>
+          </Link>
+          <Box
+            className={classes.imgTitle}
+            variant="h6"
+            align="center"
+            gutterBottom
+          >
+            여성
+          </Box>
+        </Box>
+      </Box>
+    </Box>
+  );
+}
+
+
+
+
+
+
+
+
+
 export function GenderPage() {
   const classes = useStyles();
   return (
-  <Box className={classes.root}>
-    <PC>
-      <Grid className={classes.container} spacing={3}>
-        <NavBar />
-        <Grid className={classes.emptyColumnGrid} item></Grid>
-        <Grid className={classes.emptyRowGrid} item></Grid>
-        <Grid item>
-          <GenderPageSelect />
+    <Box className={classes.root}>
+      <PC>
+        <Grid className={classes.container} spacing={3}>
+          <NavBar />
+          <Grid className={classes.emptyColumnGrid} item></Grid>
+          <Grid className={classes.emptyRowGrid} item></Grid>
+          <Grid item>
+            <GenderPageSelect />
+          </Grid>
         </Grid>
-      </Grid>
-    </PC>
+      </PC>
 
-    <Mobile>
-      Mobile
-    </Mobile>
+      <Mobile>Mobile</Mobile>
 
-    <Tablet>
-      <Grid className={classes.container} spacing={3}>
+      <Tablet>
+        <Grid className={classes.tabletContainer} spacing={3}>
+          <NavBar />
+          {/* <Grid className={classes.tabletEmptyColumnGrid} item></Grid>
+          <Grid className={classes.tabletEmptyRowGrid} item></Grid> */}
+          <Grid className={classes.tabletGenderPageGrid} item>
+            <GenderPageSelectMobile />
+          </Grid>
+        </Grid>
+      </Tablet>
+
+      <PCwide>
+        <Grid className={classes.container} spacing={3}>
           <NavBar />
           <Grid className={classes.emptyColumnGrid} item></Grid>
           <Grid className={classes.emptyRowGrid} item></Grid>
           <Grid item>
             <GenderPageSelect />
           </Grid>
-      </Grid>
-    </Tablet>
-
-    <PCwide>
-      <Grid className={classes.container} spacing={3}>
-          <NavBar />
-          <Grid className={classes.emptyColumnGrid} item></Grid>
-          <Grid className={classes.emptyRowGrid} item></Grid>
-          <Grid item>
-            <GenderPageSelect />
-          </Grid>
-      </Grid>
-    </PCwide>
-  </Box>
+        </Grid>
+      </PCwide>
+    </Box>
   );
 }
