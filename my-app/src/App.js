c64d20c9ee0bce629f53aa156903a2d7af1979d9
onshore <onshorecoding9@gmail.com>
2021-04-13 20:39:33 +0900

    <feat> : fix templates

--- my-app/src/App.js
+++ my-app/src/App.js
@@ -4,8 +4,6 @@ import { TestPage } from './pages/test_page'
 import { GenderPage } from './pages/gender_page'
 import { ResultPage } from './pages/result_page'
 import {createContext, useState} from "react"
-import './App.css';
-
 
 const GenderContext = createContext({});
 
