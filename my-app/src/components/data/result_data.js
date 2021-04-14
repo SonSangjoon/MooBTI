3e3756b3e92574538a03eaa6db6568cbcc46c215
onshore <onshorecoding9@gmail.com>
2021-04-14 10:45:00 +0900

    <feat> : Add data format

---
+++ my-app/src/components/data/result_data.js
@@ -0,0 +1,94 @@
+const resultList = {
+
+    ESFP:{ 
+        male:{
+            movie: '타이타닉',
+            name: '잭',
+            title: '항상 사랑을 말하는 ESFP 당신은, 타이타닉의 잭',
+            script: '"이 배의 탑승권을 따낸 건 내 인생 최고의 행운이었어. 당신을 만났으니까"',
+            description: '외향적이고 감성적인 ESFP 유형의 사람들은 낙천적이고 관계 맺기를 좋아해요. 그래서 연애할 때 상대방에게 사랑을 시도 때도 없이 표현하곤 하죠 😘 종종 여자친구에게 자신의 가치관을 이야기해요. 상대방이 자신을 그대로 인정해준다면 온화해지지만, 관계가 조금이라도 불편해진다면 빠르게 거리를 둔답니다. 거부당하는 걸 두려워하는 당신에게, 해주고 싶은 말이 있어요. "사랑하는 사람에게 기꺼이 상처 받을 것"이라는 말이에요.',
+            movieUrl: 'www.netflix.com/title/1181461',
+            
+            goodMbti: 'ISFJ',
+            badMbti:'INFP',
+            shortDesc: '항상 사랑을 말하는 ESFP 남자. 낙천적이고 관계 맺기를 좋아하지만, 당신과 조금이라도 관계가 불편해지면 빠르게 거리를 둬요.',
+
+        },
+        female:{
+            movie: '',
+            name: '',
+            title: '',
+            script: '',
+            description: '',
+            movieUrl: '',
+            
+            goodMbti: '',
+            badMbti: '' ,
+            shortDesc: '' , 
+        }
+    },
+    
+    ISFJ: {
+        male:{
+            movie: '',
+            name: '',
+            title: '',
+            script: '',
+            description: '',
+            movieUrl: '',
+            
+            goodMbti: '',
+            badMbti: '' ,
+            shortDesc: '' , 
+        },
+
+        female:{
+            movie: '',
+            name: '',
+            title: '',
+            script: '',
+            description: '',
+            movieUrl: '',
+            
+            goodMbti: '',
+            badMbti: '' ,
+            shortDesc: '' , 
+        }
+
+    },
+
+    INFP: {
+        male:{
+            movie: '',
+            name: '',
+            title: '',
+            script: '',
+            description: '',
+            movieUrl: '',
+            
+            goodMbti: '',
+            badMbti: '' ,
+            shortDesc: '' , 
+        },
+        
+        female:{
+            movie: '',
+            name: '',
+            title: '',
+            script: '',
+            description: '',
+            movieUrl: '',
+            
+            goodMbti: '',
+            badMbti: '' ,
+            shortDesc: '' , 
+        }
+
+    },
+
+
+
+
+}
+
+export { resultList }
