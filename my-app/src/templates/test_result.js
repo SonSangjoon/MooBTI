2ed5ff07e8e7f8ef372c630de0021866b5bbb9db
onshore <onshorecoding9@gmail.com>
2021-04-15 03:09:10 +0900

    <feat> : finish layout

--- my-app/src/templates/test_result.js
+++ my-app/src/templates/test_result.js
@@ -3,11 +3,24 @@ import { Button, Grid, Box } from "@material-ui/core";
 import { GenderContext } from "../App";
 import { useStyles } from './styles/test_result_styles'
 import TransitionsModal from '../components/modal'
+import { Mobile, PC, PCwide, Tablet } from "../components/MediaQuery";
 
 function ResultMain({data}) {
   const classes = useStyles();
   return (
     <Grid className={classes.container}>
+      <Mobile>
+      <Grid className={classes.mobileImageGrid} item>
+        <img className={classes.characterImage} src={data.imageUrl} alt="dummy" />
+      </Grid>
+      <Grid className={classes.mobileCharacterTitleGrid}>
+        <Box className={classes.mobileCharacterTitle} align="center">
+          {data.title}
+        </Box>
+      </Grid>
+      </Mobile>
+
+      <PC>
       <Grid className={classes.imageGrid} item>
         <img className={classes.characterImage} src={data.imageUrl} alt="dummy" />
       </Grid>
@@ -16,6 +29,29 @@ function ResultMain({data}) {
           {data.title}
         </Box>
       </Grid>
+      </PC>
+
+      <Tablet>
+      <Grid className={classes.tabletImageGrid} item>
+        <img className={classes.characterImage} src={data.imageUrl} alt="dummy" />
+      </Grid>
+      <Grid className={classes.tabletCharacterTitleGrid}>
+        <Box className={classes.tabletCharacterTitle} align="center">
+          {data.title}
+        </Box>
+      </Grid>
+      </Tablet>
+
+      <PCwide>
+      <Grid className={classes.imageGrid} item>
+        <img className={classes.characterImage} src={data.imageUrl} alt="dummy" />
+      </Grid>
+      <Grid className={classes.characterTitleGrid}>
+        <Box className={classes.characterTitle} align="center">
+          {data.title}
+        </Box>
+      </Grid>
+      </PCwide>
     </Grid>
   );
 }
@@ -24,8 +60,22 @@ function ResultDescription({data}) {
   const classes = useStyles();
   return (
     <Grid className={classes.container}>
-        <Grid className={classes.characterScript} item>
-          <Box className={classes.charScriptFont} align="center">
+      <Mobile>
+      <Grid className={classes.mobileCharacterScript} item>
+          <Box className={classes.mobileCharScriptText} align="center">
+            {data.script}
+          </Box>
+        </Grid>
+        <Grid className={classes.mobileCharacterDetail}>
+          <Box align="center" className={classes.mobileCharacterDetailText} >
+          {data.description}
+          </Box>
+        </Grid>
+      </Mobile>
+
+      <PC>
+      <Grid className={classes.characterScript} item>
+          <Box className={classes.charScriptText} align="center">
             {data.script}
           </Box>
         </Grid>
@@ -34,6 +84,34 @@ function ResultDescription({data}) {
           {data.description}
           </Box>
         </Grid>
+      </PC>
+
+      <Tablet>
+      <Grid className={classes.tabletCharacterScript} item>
+          <Box className={classes.tabletCharScriptText} align="center">
+            {data.script}
+          </Box>
+        </Grid>
+        <Grid className={classes.tabletCharacterDetail}>
+          <Box align="center" className={classes.tabletCharacterDetailText} >
+          {data.description}
+          </Box>
+        </Grid>
+      </Tablet>
+
+      <PCwide>
+      <Grid className={classes.characterScript} item>
+          <Box className={classes.charScriptText} align="center">
+            {data.script}
+          </Box>
+        </Grid>
+        <Grid className={classes.characterDetail}>
+          <Box align="center" className={classes.characterDetailText} >
+          {data.description}
+          </Box>
+        </Grid>
+      </PCwide>
+
     </Grid>
   );
 }
@@ -54,19 +132,69 @@ export default function ResultTemplate({data}) {
 
   return (
     <Grid className={classes.container} >
+      <PC>
+        <Grid className={classes.resultMainGrid}  item>
+          <ResultMain data={data}/>
+        </Grid>
+        <Grid className={classes.resultDescriptionGrid} item>
+          <ResultDescription data={data}/>
+        </Grid>
+        <Grid className={classes.buttonGrid} onClick ={() => setOpenModal(true)}>
+          <DataAnalysisBtn value= "어떻게 분석했어?"/>
+        </Grid>
+        <Grid className={classes.buttonGrid}>
+          <DataAnalysisBtn value= "영화보러 가기"/>
+        </Grid>
+        <TransitionsModal />
+      </PC>
+
+      <Mobile>
+        <Grid className={classes.mobileResultMainGrid}  item>
+          <ResultMain data={data}/>
+        </Grid>
+        <Grid className={classes.resultDescriptionGrid} item>
+          <ResultDescription data={data}/>
+        </Grid>
+        <Grid className={classes.buttonGrid} onClick ={() => setOpenModal(true)}>
+          <DataAnalysisBtn value= "어떻게 분석했어?"/>
+        </Grid>
+        <Grid className={classes.buttonGrid}>
+          <DataAnalysisBtn value= "영화보러 가기"/>
+        </Grid>
+        <TransitionsModal />
+      </Mobile>
+
+      <Tablet>
       <Grid className={classes.resultMainGrid}  item>
-        <ResultMain data={data}/>
-      </Grid>
-      <Grid className={classes.resultDescriptionGrid} item>
-        <ResultDescription data={data}/>
-      </Grid>
-      <Grid className={classes.buttonGrid} onClick ={() => setOpenModal(true)}>
-        <DataAnalysisBtn value= "어떻게 분석했어?"/>
-      </Grid>
-      <Grid className={classes.buttonGrid}>
-        <DataAnalysisBtn value= "영화보러 가기"/>
-      </Grid>
-      <TransitionsModal />
+          <ResultMain data={data}/>
+        </Grid>
+        <Grid className={classes.resultDescriptionGrid} item>
+          <ResultDescription data={data}/>
+        </Grid>
+        <Grid className={classes.buttonGrid} onClick ={() => setOpenModal(true)}>
+          <DataAnalysisBtn value= "어떻게 분석했어?"/>
+        </Grid>
+        <Grid className={classes.buttonGrid}>
+          <DataAnalysisBtn value= "영화보러 가기"/>
+        </Grid>
+        <TransitionsModal />
+      </Tablet>
+
+      <PCwide>
+        <Grid className={classes.resultMainGrid}  item>
+          <ResultMain data={data}/>
+        </Grid>
+        <Grid className={classes.resultDescriptionGrid} item>
+          <ResultDescription data={data}/>
+        </Grid>
+        <Grid className={classes.buttonGrid} onClick ={() => setOpenModal(true)}>
+          <DataAnalysisBtn value= "어떻게 분석했어?"/>
+        </Grid>
+        <Grid className={classes.buttonGrid}>
+          <DataAnalysisBtn value= "영화보러 가기"/>
+        </Grid>
+        <TransitionsModal />
+      </PCwide>
     </Grid>
   );
 }
