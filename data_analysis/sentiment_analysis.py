1056a2f88cdd8ffce6a4bb3a07496944dbe0c243
Minho Lee <lkjjr0424@hanmail.net>
2021-04-15 15:33:50 +0900

    [UPDATE] update character sentiment analysis

--- data_analysis/sentiment_analysis.py
+++ data_analysis/sentiment_analysis.py
@@ -1,28 +1,19 @@
 from nrclex import NRCLex
-import matplotlib.pyplot as plt
 import numpy as np
+import csv
+
+# if file type csv
+text = ""
+with open("INCEPTION_ARIADNE.csv") as file:
+    lines = csv.reader(file)
+    for line in lines:
+        text += line[1]
 
 # change scripts
 text_object = NRCLex("""""")
 
-labels = np.array(list(text_object.raw_emotion_scores.keys()))
-stats = list(text_object.raw_emotion_scores.values())
-stats = np.array([round((i / sum(stats) * 100), 2) for i in stats])
-print(labels)  # check label
-print(stats)  # check stats
-
-angles = np.linspace(0, 2 * np.pi, len(labels), endpoint=False)
-# close the plot
-stats = np.concatenate((stats, [stats[0]]))
-angles = np.concatenate((angles, [angles[0]]))
-
-fig = plt.figure(figsize=(10, 6))
-ax = fig.add_subplot(111, polar=True)
-ax.plot(angles, stats, "o-", linewidth=2)
-ax.fill(angles, stats, alpha=0.25)
-ax.set_thetagrids(angles * 180 / np.pi, labels)
-ax.grid(True)
-
-# save radar chart
-fig.savefig("./result/name.png")  # change name to character name
-
+labels = np.array(["joy", "sadness", "anger", "disgust", "fear"])
+stats = np.array([text_object.raw_emotion_scores[i] for i in labels])
+stats = np.array([int(round(i / stats.sum() * 100)) for i in stats])
+print(labels)
+print(stats)
