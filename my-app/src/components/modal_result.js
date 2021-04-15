9b106e2b480615e0c50e952fa56bb12e7d074c77
YUZAMIN <untmd.crmn@gmail.com>
2021-04-16 02:12:41 +0900

    <feat> : add Modal

--- my-app/src/components/modal_result.js
+++ my-app/src/components/modal_result.js
@@ -8,6 +8,7 @@ import { GenderContext } from "../App";
 import IconButton from "@material-ui/core/IconButton";
 import CloseIcon from "@material-ui/icons/Close";
 import { Mobile, Tablet, PC, PCwide } from "../components/MediaQuery.js";
+// import happy from '/images/sentiment/기쁨이.png'
 
 const useStyles = makeStyles((theme) => ({
   root: {
@@ -15,33 +16,141 @@ const useStyles = makeStyles((theme) => ({
   },
   modal: {
     display: "flex",
-    alignItems: "center",
+    // alignItems: "center",
     justifyContent: "center",
+    // position: "fixed",
+    // left: "50%"
+
+    // position: "absolute",
+    // left: "50%",
+    // top: "50%",
+    // transform: "translate(-50%, -50%)"
+    alignItems: "center",
   },
   container: {
     display: "grid",
     gridTemplateRows: "repeat(12,1fr)",
-    gridTemplateColumns: "repeat(12,1fr)",
+    gridTemplateColumns: "repeat(13,1fr)",
     backgroundColor: "black",
     border: "2px solid red",
     boxShadow: theme.shadows[1],
-    padding: theme.spacing(2, 4, 3),
-    width: "768px",
-    height: "80vh",
+    // padding: theme.spacing(3,0),
+    maxWidth: "768px",
+    width: "100%",
+    height: "85vh",
   },
 
-  modalGrid: {
-    gridColumn: "span 12",
-    gridRow: "span 12",
+  titleGrid: {
+    gridColumn: "span 13",
+    gridRow: "2/span 1",
   },
 
+  titleText: {
+    color: "white",
+    fontFamily: "S-CoreDream-6Bold",
+    fontSize: "32px",
+  },
+
+  subTitleGrid: {
+    gridColumn: "span 13",
+    gridRow: "3/span 1",
+  },
+
+  subTitleText: {
+    color: "white",
+    fontFamily: "S-CoreDream-4Regular",
+    fontSize: "17px",
+  },
+  happyCircleGrid: {
+    gridColumn: "2/span 3",
+    gridRow: "4/span 4",
+  },
+  sadCircleGrid: {
+    gridColumn: "6/span 3",
+    gridRow: "4/span 4",
+  },
+  angryCircleGrid: {
+    gridColumn: "10/span 3",
+    gridRow: "4/span 4",
+  },
+  fussyCircleGrid: {
+    gridColumn: "4/span 3",
+    gridRow: "8/span 4",
+  },
+  timidCircleGrid: {
+    gridColumn: "8/span 3",
+    gridRow: "8/span 4",
+  },
+  emotionImage: {
+    objectFit: "fill",
+    width: "80%",
+    // maxHeight: "100%",
+  },
+
+  emotionText: {
+    color: "white",
+    fontFamily:"S-CoreDream-4Regular"
+  },
+  analysisGrid: {
+    gridColumn: "span 13",
+    gridRow: "12/span 1",
+  },
+  analysisText: {
+    color: "white",
+    fontSize: "18px",
+    fontFamily:"S-CoreDream-4Regular"
+  },
+  modalCloseBtnGrid: {
+    gridColumn: "13",
+    gridRow: "span 1",
+  },
   closeButton: {
-    // position: 'absolute',
-    // right: theme.spacing(1),
-    // top: theme.spacing(1),
     color: theme.palette.grey[500],
+    align: "right",
   },
 }));
+
+function Title(data) {
+  const classes = useStyles();
+  return (
+    <Box className={classes.titleText} align="center">
+      {data.text}
+    </Box>
+  );
+}
+
+function SubTitle(data) {
+  const classes = useStyles();
+  return (
+    <Box className={classes.subTitleText} align="center">
+      {data.text}
+    </Box>
+  );
+}
+
+function EmotionCircle(data) {
+  const classes = useStyles();
+
+  // src="/images/sentiment/happy.png"
+  const imageUrl = `/images/emotion/${data.gender}/${data.emotion}.png`;
+  return (
+    <Box align="center">
+      <img src={imageUrl} alt="Logo" className={classes.emotionImage} />
+      <Box className={classes.emotionText} align="center">
+        {data.number}명의 {data.name}
+      </Box>
+    </Box>
+  );
+}
+
+function Analysis(data) {
+  const classes = useStyles();
+  return (
+    <Box className={classes.analysisText} align="center">
+      {data.text}
+    </Box>
+  );
+}
 
 function ModalCloseBtn() {
   const classes = useStyles();
@@ -62,77 +171,26 @@ function ModalCloseBtn() {
   );
 }
 
-// function ModalWindow() {
-//   const classes = useStyles();
-//   const { openModal, setOpenModal } = useContext(GenderContext);
-
-//   const handleClose = () => {
-//     setOpenModal(false);
-//   };
-
-//   return (
-//     <Modal
-//       className={classes.modal}
-//       open={openModal}
-//       onClose={handleClose}
-//       closeAfterTransition
-//       BackdropComponent={Backdrop}
-//       BackdropProps={{
-//         timeout: 500,
-//       }}
-//     >
-//       <Fade in={openModal}>
-//         <div className={classes.paper}>
-//           <h2 id="transition-modal-title" color="white">
-//             Transition modal
-//           </h2>
-//           <p id="transition-modal-description">
-//             react-transition-group animates me.
-//           </p>
-//           <ModalCloseBtn />
-//         </div>
-//       </Fade>
-//     </Modal>
-//   );
-// }
-
-// function ModalWindow() {
-//   const classes = useStyles();
-//   const { openModal, setOpenModal } = useContext(GenderContext);
-
-//   const handleClose = () => {
-//     setOpenModal(false);
-//   };
-
-//   return (
-//     <Fade in={openModal}>
-//       <Grid className={classes.paper}>
-//         <h2 id="transition-modal-title" color="white">
-//           Transition modal
-//         </h2>
-//         <p id="transition-modal-description">
-//           react-transition-group animates me.
-//         </p>
-//         <ModalCloseBtn />
-//       </Grid>
-//     </Fade>
-//   );
-// }
-
-
-
-
-
-
-
-
-export default function TransitionsModal() {
+export default function TransitionsModal(data) {
   const classes = useStyles();
   const { openModal, setOpenModal } = useContext(GenderContext);
 
   const handleClose = () => {
     setOpenModal(false);
   };
+
+  const titleText = "ESFP 잭이 로즈를 사로잡은 비결 ♥";
+  const subTitleText =
+    "잭의 대사를 분석해서 머릿 속 감정 친구들 100명을 찾아냈어요!";
+  const analysisText =
+    "\"ESFP 머릿 속 42명의 기쁨이가 애정을 표현하는 데에 열일했어요!\"";
+
+  const gender = "male";
+  const happyNumber = 10;
+  const sadNumber = 20;
+  const angryNumber = 30;
+  const fussyNumber = 40;
+  const timidNumber = 50;
 
   return (
     <Modal
@@ -149,14 +207,241 @@ export default function TransitionsModal() {
         <PC>
           <Fade in={openModal}>
             <Grid className={classes.container}>
-              <ModalCloseBtn />
+              <Grid className={classes.titleGrid}>
+                <Title text={titleText} />
+              </Grid>
+              <Grid className={classes.subTitleGrid}>
+                <SubTitle text={subTitleText} />
+              </Grid>
+              <Grid className={classes.happyCircleGrid}>
+                <EmotionCircle
+                  name="기쁨이"
+                  gender={gender}
+                  number={happyNumber}
+                  emotion="happy"
+                />
+              </Grid>
+              <Grid className={classes.sadCircleGrid}>
+                <EmotionCircle
+                  name="슬픔이"
+                  gender={gender}
+                  number={sadNumber}
+                  emotion="sad"
+                />
+              </Grid>
+              <Grid className={classes.angryCircleGrid}>
+                <EmotionCircle
+                  name="버럭이"
+                  gender={gender}
+                  number={angryNumber}
+                  emotion="angry"
+                />
+              </Grid>
+              <Grid className={classes.fussyCircleGrid}>
+                <EmotionCircle
+                  name="까칠이"
+                  gender={gender}
+                  number={fussyNumber}
+                  emotion="fussy"
+                />
+              </Grid>
+              <Grid className={classes.timidCircleGrid}>
+                <EmotionCircle
+                  name="소심이"
+                  gender={gender}
+                  number={timidNumber}
+                  emotion="timid"
+                />
+              </Grid>
+              <Grid className={classes.analysisGrid}>
+                <Analysis text={analysisText} />
+              </Grid>
+              <Grid className={classes.modalCloseBtnGrid}>
+                <ModalCloseBtn />
+              </Grid>
             </Grid>
           </Fade>
         </PC>
 
-        <Tablet></Tablet>
-        <Mobile></Mobile>
-        <PCwide></PCwide>
+        {/* Tablet View */}
+        <Tablet>
+          <Fade in={openModal}>
+            <Grid className={classes.container}>
+              <Grid className={classes.titleGrid}>
+                <Title text={titleText} />
+              </Grid>
+              <Grid className={classes.subTitleGrid}>
+                <SubTitle text={subTitleText} />
+              </Grid>
+              <Grid className={classes.happyCircleGrid}>
+                <EmotionCircle
+                  name="기쁨이"
+                  gender={gender}
+                  number={happyNumber}
+                  emotion="happy"
+                />
+              </Grid>
+              <Grid className={classes.sadCircleGrid}>
+                <EmotionCircle
+                  name="슬픔이"
+                  gender={gender}
+                  number={sadNumber}
+                  emotion="sad"
+                />
+              </Grid>
+              <Grid className={classes.angryCircleGrid}>
+                <EmotionCircle
+                  name="버럭이"
+                  gender={gender}
+                  number={angryNumber}
+                  emotion="angry"
+                />
+              </Grid>
+              <Grid className={classes.fussyCircleGrid}>
+                <EmotionCircle
+                  name="까칠이"
+                  gender={gender}
+                  number={fussyNumber}
+                  emotion="fussy"
+                />
+              </Grid>
+              <Grid className={classes.timidCircleGrid}>
+                <EmotionCircle
+                  name="소심이"
+                  gender={gender}
+                  number={timidNumber}
+                  emotion="timid"
+                />
+              </Grid>
+              <Grid className={classes.analysisGrid}>
+                <Analysis text={analysisText} />
+              </Grid>
+              <Grid className={classes.modalCloseBtnGrid}>
+                <ModalCloseBtn />
+              </Grid>
+            </Grid>
+          </Fade>
+        </Tablet>
+
+        {/* Mobile View */}
+        <Mobile>
+          <Fade in={openModal}>
+            <Grid className={classes.container}>
+              <Grid className={classes.titleGrid}>
+                <Title text={titleText} />
+              </Grid>
+              <Grid className={classes.subTitleGrid}>
+                <SubTitle text={subTitleText} />
+              </Grid>
+              <Grid className={classes.happyCircleGrid}>
+                <EmotionCircle
+                  name="기쁨이"
+                  gender={gender}
+                  number={happyNumber}
+                  emotion="happy"
+                />
+              </Grid>
+              <Grid className={classes.sadCircleGrid}>
+                <EmotionCircle
+                  name="슬픔이"
+                  gender={gender}
+                  number={sadNumber}
+                  emotion="sad"
+                />
+              </Grid>
+              <Grid className={classes.angryCircleGrid}>
+                <EmotionCircle
+                  name="버럭이"
+                  gender={gender}
+                  number={angryNumber}
+                  emotion="angry"
+                />
+              </Grid>
+              <Grid className={classes.fussyCircleGrid}>
+                <EmotionCircle
+                  name="까칠이"
+                  gender={gender}
+                  number={fussyNumber}
+                  emotion="fussy"
+                />
+              </Grid>
+              <Grid className={classes.timidCircleGrid}>
+                <EmotionCircle
+                  name="소심이"
+                  gender={gender}
+                  number={timidNumber}
+                  emotion="timid"
+                />
+              </Grid>
+              <Grid className={classes.analysisGrid}>
+                <Analysis text={analysisText} />
+              </Grid>
+              <Grid className={classes.modalCloseBtnGrid}>
+                <ModalCloseBtn />
+              </Grid>
+            </Grid>
+          </Fade>
+        </Mobile>
+
+        {/* PCwide View */}
+        <PCwide>
+          <Fade in={openModal}>
+            <Grid className={classes.container}>
+              <Grid className={classes.titleGrid}>
+                <Title text={titleText} />
+              </Grid>
+              <Grid className={classes.subTitleGrid}>
+                <SubTitle text={subTitleText} />
+              </Grid>
+              <Grid className={classes.happyCircleGrid}>
+                <EmotionCircle
+                  name="기쁨이"
+                  gender={gender}
+                  number={happyNumber}
+                  emotion="happy"
+                />
+              </Grid>
+              <Grid className={classes.sadCircleGrid}>
+                <EmotionCircle
+                  name="슬픔이"
+                  gender={gender}
+                  number={sadNumber}
+                  emotion="sad"
+                />
+              </Grid>
+              <Grid className={classes.angryCircleGrid}>
+                <EmotionCircle
+                  name="버럭이"
+                  gender={gender}
+                  number={angryNumber}
+                  emotion="angry"
+                />
+              </Grid>
+              <Grid className={classes.fussyCircleGrid}>
+                <EmotionCircle
+                  name="까칠이"
+                  gender={gender}
+                  number={fussyNumber}
+                  emotion="fussy"
+                />
+              </Grid>
+              <Grid className={classes.timidCircleGrid}>
+                <EmotionCircle
+                  name="소심이"
+                  gender={gender}
+                  number={timidNumber}
+                  emotion="timid"
+                />
+              </Grid>
+              <Grid className={classes.analysisGrid}>
+                <Analysis text={analysisText} />
+              </Grid>
+              <Grid className={classes.modalCloseBtnGrid}>
+                <ModalCloseBtn />
+              </Grid>
+            </Grid>
+          </Fade>
+        </PCwide>
       </Box>
     </Modal>
 
