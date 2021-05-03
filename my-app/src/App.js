6b1081984f30257a8f4f1fabb06ead5b14adb5f6
YUZAMIN <untmd.crmn@gmail.com>
2021-05-03 11:45:53 +0900

    <feat> : Add new Intro page and Gender select page

--- my-app/src/App.js
+++ my-app/src/App.js
@@ -4,6 +4,11 @@ import { TestPage } from "./pages/test_page";
 import { GenderPage } from "./pages/gender_page";
 import { ResultPage } from "./pages/result_page";
 import { createContext, useState } from "react";
+
+
+import { NewIntro } from "./new/new_intro";
+import { NewGenderSelect } from "./new/new_gender_select";
+
 import "./App.css";
 const MbtiContext = createContext({});
 
@@ -25,8 +30,10 @@ export function App() {
         }}
       >
         <BrowserRouter>
-          <Route exact path="/" component={IntroPage} />
-          <Route exact path="/gender" component={GenderPage} />
+          <Route exact path="/" component={NewIntro} />
+          {/* <Route exact path="/" component={IntroPage} /> */}
+          <Route exact path="/gender" component={NewGenderSelect} />
+          {/* <Route exact path="/gender" component={GenderPage} /> */}
           <Route exact path="/test" component={TestPage} />
           <Route exact path="/:mbti/:gender" component={ResultPage} />
         </BrowserRouter>
