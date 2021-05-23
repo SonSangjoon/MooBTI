2d511282a6903cd3d0314245ff25d2300efb10ed
onshore <onshorecoding9@gmail.com>
2021-05-23 16:36:07 +0900

    fix: mbti box

--- my-app/src/templates/test_result_mobile.js
+++ my-app/src/templates/test_result_mobile.js
@@ -18,7 +18,8 @@ function ResultMain({ data }) {
             alt="dummy"
           />
         </Box>
-        <Box className={classes.mobileCharacterMbtiBox}>{data.mbti}</Box>
+        <Box className={classes.mobileCharacterMbtiBox}><Box>{data.mbti}
+          </Box></Box>
         <Box className={classes.mobileCharacterTitle} align="center">
           {data.title}
         </Box>
